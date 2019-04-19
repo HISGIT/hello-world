57e1a4723bef7888a3e5733936f300f11c6a1724
HISGIT <totorystal@yahoo.com>
2019-04-19 09:41:32 +0800

    add Excercise-list.c

---
+++ src/Excercise-list.c
@@ -0,0 +1,3 @@
+Exercise 1-15 Rewrite the temperature conversion program of Section 1.2 (in .c file c1_3(2).c) to use a function for conversion.
+Exercise 1-16 
+Exercise 1-17 Write a program to print all input lines that are longer then 80 characters.
