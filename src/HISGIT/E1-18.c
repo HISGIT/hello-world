d9322f8f1d8280d2a3d8a4471c53e884fe831b7c
HISGIT <totorystal@yahoo.com>
2019-04-30 20:33:27 +0800

    adjust the function removeBnkLine

--- src/HISGIT/E1-18.c
+++ src/HISGIT/E1-18.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #define MAXLEN 20
-/*version 1.0: write a program to remove  trailing blanks and tabs from each line of input, and to delete entirely blanks lines*/
-void removeBnkLine(char inchar[]);
+/*version 1.1
+ write a program to remove  trailing blanks and tabs from each line of input, and to delete entirely blanks lines*/
+void removeBnkLine(char inchar[],int outputLen);
 main()
 {	int c,k;
 	c = 0;
@@ -12,24 +13,24 @@ main()
 		if(k != ' ' && k != '\t')
 		{
 			line[c] = k;
-			/*printf("line[%d]:%d\n",c,line[c]);*/
+		/*	printf("line[%d]:%d\n",c,line[c]);*/
 			c++;
 		}
 		if(c == MAXLEN )
 		{
-			removeBnkLine(line);
+			removeBnkLine(line,MAXLEN);
 			c = 0;
 		}
 	}
-	removeBnkLine(line);
+	removeBnkLine(line,c);/*output the last cycle*/
 	return 0;	
 }
 
 
-void removeBnkLine(char inchar[])
+void removeBnkLine(char inchar[],int outputLen)
 {
 	int i;i = 0;
-	while(i < MAXLEN )
+	while(i < outputLen )
 	{	
 		if(inchar[i - 1] != '\n' || inchar[i] != '\n')
 		printf("%c",inchar[i]);
