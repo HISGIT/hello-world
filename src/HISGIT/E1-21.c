db31f947585ff287266cba604317ad25381de862
HISGIT <totorystal@yahoo.com>
2019-05-06 22:21:11 +0800

    Exercise 1-21

---
+++ src/HISGIT/E1-21.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+/*write a program entab that replaces strings of blanks with the minium number of tabs and blanks to achieve the same spacing. Use the same stops as for detab. When either a tab or a single blank suffice to reach a tab stop, which
+should be given preference?*/
