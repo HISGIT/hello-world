5aa7b11f83befedc31b021ccc139289122f8e8a3
HISGIT <totorystal@yahoo.com>
2020-02-14 12:35:46 +0800

    Excercise 3-2

--- src/HISGIT/E3-2.c
+++ src/HISGIT/E3-2.c
@@ -2,7 +2,66 @@
  * newline and tab into visiable excape sequences like \n and \t as it copies 
  * the string t to s. Use a switch . Write a function for the other direction 
  * as well, converting escape sequences into the real chracters. */
+#include <stdio.h>
+#define MAXLINE 1000
 
-void main(){
-
+int escape(char s[], char t[]){
+    int i, j;
+    for(i = 0, j = 0; s[i] != '\0' ; i++){
+       switch(s[i]){
+           case '\n':
+               t[j++] = '\\';
+               t[j++] = 'n';
+               break;
+           case '\t':
+               t[j++] = '\\';
+               t[j++] = 't';
+               break;
+           default:
+               t[j++] = s[i];
+       }
+    }
+    t[j] = '\0';
+    return 0;
+}
+int rvrescape(char s[], char t[]){
+    int i, j;
+    for(i = 0, j = 0; s[i] != '\0' ; i++){
+       switch(s[i]){
+           case '\\':
+               switch(s[++i]){
+                case 'n':
+                    t[j++] = '\n';
+                    break;
+                case 't':
+                    t[j++] = '\t';
+                    break;
+               }
+               break;
+           default:
+               t[j++] = s[i];
+               break;
+       }
+    }
+    t[j] = '\0';
+    return 0;
+}
+int getlines(char line[], int len){
+    int i,c;
+    for(i = 0; (c = getchar()) != EOF && i < len -1 && c != '\n';i++){
+        line[i] = c;
+    }
+    if(c == '\n'){
+        line[i] = c;
+        i++;
+    }
+    line[i] = '\0';
+    return i;
+}
+int main(){
+    char s[MAXLINE], t[MAXLINE];
+    getlines(s, MAXLINE);
+    rvrescape(s, t);
+    printf("%s",t);
+    return 0;
 }
