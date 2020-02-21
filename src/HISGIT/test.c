25263e2788ff2742773480b3f0a636c8a247d9ab
HISGIT <totorystal@yahoo.com>
2020-02-21 14:24:53 +0800

    p70 experiment

--- src/HISGIT/test.c
+++ src/HISGIT/test.c
@@ -1,10 +1,19 @@
+/*shell sort*/
 #include <stdio.h>
-main()
-{	
-/*	int i9 = 9;int ic9 = '9';*/
-	int i = 0;
-	while (i <= 201)
-		printf("\t%c\t%d\n",i,i);
-		i = i+1;
-	}
+#define MAXLINE 1000
+int getlines(char line[], int max);
+int strindex(char source[], char search[]);
+char pattern[] = "ould";
+int main(void){
+    char line[MAXLINE];
+    int found = 0;
+
+    while(getlines(line, MAXLINE) > 0){
+        if(strindex(line, pattern) >= 0){
+            printf("%s",line);
+            found++;
+        }
+    }
+    return found;
 }
+
